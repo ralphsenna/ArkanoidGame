3838e2b4249be0519ad1bbb7a53755eff0d510b9
ralphsenna <103199315+ralphsenna@users.noreply.github.com>
2022-06-10 00:09:36 -0300

    Commit Inicial

---
+++ src/Principal/ArkanoidEstrutura/Raquete.java
@@ -0,0 +1,50 @@
+package Principal.ArkanoidEstrutura;
+
+import javafx.event.EventHandler;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+
+public class Raquete extends GameObject {
+
+    public Raquete(int cx, int cy, Pane painel) {
+        super(cx, cy, painel);
+    }
+    
+    @Override
+    public Rectangle getFigura() {
+        return (Rectangle) super.getFigura();
+    }
+
+    //Move a raquete com o movimento do mouse
+    @Override
+    public void mover() {
+        super.getPainel().setOnMouseMoved(new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(javafx.scene.input.MouseEvent event) {
+                if(event.getX()>=(getFigura().getWidth()/2) && event.getX()<=(getPainel().getWidth() - getFigura().getWidth()/2))
+                    setCx((int) (event.getX()-getFigura().getWidth()/2));
+                    getFigura().setX(getCx());
+            }
+        });
+    }
+    
+    //Impede o reconhecimento do mouse para mecher a raquete enquanto esta pausado
+    public void pausar() {
+        super.getPainel().setOnMouseMoved(new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(javafx.scene.input.MouseEvent event) {
+            }
+        });
+    }
+    
+    //Aumenta o tamanho da raquete
+    public void aumentar(int px) {
+        getFigura().setWidth(getFigura().getWidth() + px);
+    }
+    
+    //Volta a raquete ao tamanho original
+    public void normalizar() {
+        getFigura().setWidth(110);
+    }
+}
